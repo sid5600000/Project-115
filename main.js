689f5f33e1af2f30d65384ed24fee2bd81013e42
sid5600000 <86678953+sid5600000@users.noreply.github.com>
2022-07-28 11:26:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function draw(){
+    image(video,0,0,300,300);
+}
+
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        nose_x = results[0].pose.nose.x;
+        nose_y = results[0].pose.nose.y;
+        console.log("The X value of your nose is : " + nose_x);
+        console.log("The Y value of your nose is : " + nose_y);
+    }
+}
+
+
+function take_snapshot(){
+    save("myFilterImage.png");j
+}
+
+function modelLoaded(){
+    console.log("Posenet is Initialized");
+}
